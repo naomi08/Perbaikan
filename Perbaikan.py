1849c3cd5557b9390b707181983f87d423820cbf
naomi08 <57443647+naomi08@users.noreply.github.com>
2019-11-10 23:24:59 +0800

    Add files via upload

---
+++ Perbaikan.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Nama :Naomi Ajmasaru
+Nim  :17021106114
+
+
+# In[ ]:
+
+
+TUTORIAL (PENJELASAN SINGKAT) MENGENAI PROSES PEMBUATAN TUGAS DI BAWAH INI.
+
+
+# In[3]:
+
+
+import sympy as sym
+sym.init_printing()
+a, b, x, y,  z, t, w, = sym.symbols('a b x y z t w')
+
+
+# Perlu di jelaskan bahwa, dibawah ini merupakan code yang mana hasilnya akan menjadi soal.
+# Diamana soal tersebut sudah menjadi huruf contoh seperti dibawah ini.
+# 
+
+# In[4]:
+
+
+print ('1')
+sym.Integral (4*x**6-2*x**3+7*x-4)
+
+
+# In[5]:
+
+
+print ('Jawab')
+sym.integrate (4*x**6-2*x**3+7*x-4)
+
+
+# In[ ]:
+
+
+Penjelasan singkat:
+1. Tanda * merupakan X
+2. Tanda** Merupakan pangkat.
+
+
+# In[6]:
+
+
+print ('soal nomor 6')
+sym.Integral (3*sym.sqrt(w)+10*sym.sqrt(w**3))
+
+
+# In[10]:
+
+
+print ('nomor 11')
+sym.integrate (sym.sqrt(z)*(z**2-1/(4*z)))
+
+
+# In[6]:
+
+
+from sympy.abc import x
+
+
+# In[17]:
+
+
+x=4
+((2*x**2+x-15)/(x**2+7*x+12),x,4)
+
+
+# In[7]:
+
+
+3/8
+
+
+# In[19]:
+
+
+
+import sympy
+from sympy import limit
+from sympy.abc import x
+
+
+# In[25]:
+
+
+print ("1")
+limit((2*x**2+x-15)/(x**2+7*x+12),x,4)
+
+
+# In[28]:
+
+
+print ("2")
+limit((3*x**2-14*x+8)/(x**2-3*x-4),x,4)
+
+
+# In[8]:
+
+
+print('16')
+sym.integrate (12 + csc(x)*(sin(x)+)))
+
+
+# In[ ]:
+
+
+
+
